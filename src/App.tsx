ef9525f9c7a2c90a07840f5a9a213b0b47c14ab9
rathodvikramk382@gmail.com <rathodvikramk382@gmail.com>
2023-12-09 01:18:54 +0530

    app.tsx updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import Todo from "./components/Todo/todo";
 
-const Mytoitems = [
-  {
-    id: 1,
-    title: "this is first task",
-  },
-  {
-    id: 2,
-    title: "this is 2nd task",
-  },
-  {
-    id:3,
-    title: "this is 3rd task",
-  },
-];
-
 const App: React.FC = () => {
+  const [tasks, setTasks] = useState<any[]>([]); // State to store tasks
+  const [taskInput, setTaskInput] = useState<string>(''); // State to store input value
+
+  const handleAddTask = () => {
+    if (taskInput.trim() !== '') {
+      const newTask = {
+        id: tasks.length + 1,
+        title: taskInput,
+      };
+      setTasks([...tasks, newTask]);
+      setTaskInput(''); // Clear input after adding task
+    }
+  };
+
   return (
     <div>
-      <Todo  items={Mytoitems}/>
-      
+      <input
+        type="text"
+        placeholder="Enter task"
+        value={taskInput}
+        onChange={(e) => setTaskInput(e.target.value)}
+        onKeyPress={(e) => {
+          if (e.key === "Enter") {
+            handleAddTask();
+          }
+        }}
+      />
+      <button onClick={handleAddTask}>Add Task</button>
+      <Todo items={tasks} />
     </div>
   );
 };
